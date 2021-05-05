d871d6523e532d8cb7df078ccad38d4ff8e45790
NickAzz <nick.azurin@gmail.com>
2021-05-05 19:27:03 +0000

    entered inital entering function

--- client.js
+++ client.js
@@ -1,4 +1,10 @@
 const net = require("net");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const connect = function () {
   const conn = net.createConnection({
@@ -6,14 +12,23 @@ const connect = function () {
     port: 50541,
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Connected to the game succesfully!");
+  })
+
+  conn.on('connect', () => {
+    rl.question("Enter a 3 letter initial: ", (initials) => {
+      conn.write(`Name: ${initials}`);
+    } )
+  })
+
   conn.on("data", (data) => {
-    console.log(data);
+    console.log(`\n ${data}`);
   });
-  
+
   return conn;
 };
 
-module.exports = {connect}
+module.exports = { connect }
