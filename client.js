a562aed5788642788ca521cb8551b40e23c7e8ad
NickAzz <nick.azurin@gmail.com>
2021-05-05 19:37:36 +0000

    added movements function

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 const net = require("net");
 const readline = require('readline');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -11,18 +10,19 @@ const connect = function () {
     host: `localhost`,
     port: 50541,
   });
-
   conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Connected to the game succesfully!");
-  })
+  console.log("Connected to the game succesfully!");
 
   conn.on('connect', () => {
     rl.question("Enter a 3 letter initial: ", (initials) => {
       conn.write(`Name: ${initials}`);
+      movements();
     } )
   })
+
+  const movements = () => {
+ 
+  }
 
   conn.on("data", (data) => {
     console.log(`\n ${data}`);
